6690213837b47e1cb9c66b994aa37abb76b8ad65
Gatien Haddad <le.magicien.annonyme@gmail.com>
2024-06-24 16:01:55 +0200

    first commit

---
+++ scripts/script.js
@@ -0,0 +1,9 @@
+div_title = document.querySelector('.titre');
+
+
+
+
+// Au lancement de la page
+document.addEventListener('DOMContentLoaded', function() {
+    div_title.innerHTML = "PAGE_LOADED";
+});
